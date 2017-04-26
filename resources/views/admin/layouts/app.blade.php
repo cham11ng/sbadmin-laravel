9c32b5387c445a5e596a8034e609ce3d77bb60e6
Sagar Chamling <sgr.raee@gmail.com>
2017-04-26 10:50:07 +0545

    Change title and application name in views and .env.example

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'SB Admin 2 | cham11ng') }}</title>
 
-    <link rel="stylesheet" href="{{ asset("css/app.css") }}"/>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
     <link rel="stylesheet" href="{{ asset('css/timeline.css') }}">
 
